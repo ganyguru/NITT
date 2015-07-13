936b514ed98f31711db1e7cf19a53275c2218d7a
ganyguru <cprganesh@hotmail.com>
2015-07-13 10:38:46 +0530

    latest commit

---
+++ events.php
@@ -0,0 +1,88 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+<link rel="stylesheet" type="text\stylesheet" href="css/sidebar.css"/>
+<link rel="stylesheet" type="text\stylesheet" href="css/cards.css"/>
+<link rel="stylesheet" href="css/responsivemobilemenu.css" type="text/css">
+
+<script type="text/javascript" src="script/responsivemobilemenu.js"></script>
+	<script src="js/script.js"></script>
+</head>
+<body>
+	<div class ="wrapper">
+		
+		
+		
+			<img class='img-responsive' src="img/slider1.png" style="background:#2d2d2f;display: block;max-width: 100%;width: 100%;max-height:355px;height: auto;" />		<center>
+		<div class="rmm minimal" data-menu-style="minimal" style="max-width: 100%;background:2d2d2f;"><div class="rmm-toggled rmm-closed" style="display: none;"><div class="rmm-toggled-controls"><div class="rmm-toggled-title">Menu</div><div class="rmm-button"><span>&nbsp;</span><span>&nbsp;</span><span>&nbsp;</span></div></div><ul style="display: none;">
+            <li><a href="http://responsivemobilemenu.com/demo/#home">Home</a></li>
+            <li><a href="http://responsivemobilemenu.com/demo/#about-me">About me</a></li>
+            <li><a href="http://responsivemobilemenu.com/demo/#gallery">Gallery</a></li
+            <li><a href="http://responsivemobilemenu.com/demo/#blog">Blog</a></li>
+            <li><a href="http://responsivemobilemenu.com/demo/#links">Links</a></li>
+            <li><a href="http://responsivemobilemenu.com/demo/#sitemap">Sitemap</a></li> 
+       </ul></div>
+        <ul class="rmm-main-list" style="display: block;">
+            <li><a href="http://responsivemobilemenu.com/demo/#home">Home</a></li>
+            <li><a href="http://responsivemobilemenu.com/demo/#about-me">About me</a></li>
+            <li><a href="http://responsivemobilemenu.com/demo/#gallery">Gallery</a></li>
+            <li><a href="http://responsivemobilemenu.com/demo/#blog">Blog</a></li>
+            <li><a href="http://responsivemobilemenu.com/demo/#links">Links</a></li>
+            <li><a href="http://responsivemobilemenu.com/demo/#sitemap">Sitemap</a></li> 
+       </ul>
+</div>
+		
+		<div id="sidebar">
+
+		<nav>
+		<div class="rot">
+		Menu
+		</div>
+  		  <ul>
+		    <li><a href="#">Home</a></li>
+		    <li><a href="#">Academic</a></li>
+		    <li><a href="#">Departments</a></li>
+		    <li><a href="#">Contact</a></li>
+  		</ul></nav>
+		</div>
+
+	<center>
+		<div class="ctemp">
+		</div>
+	<div class="scon">
+
+				<div class="bar">
+					<p class="heading orange">NOTICES/DOWNLOADS </p><hr>
+					<p class="normal link ">UG/PG : Answer scripts revaluation and Course registration </p><hr>
+					<p class="normal link ">UG/PG : Answer scripts revaluation and Course registration </p><hr>
+					<p class="normal link ">UG/PG : Answer scripts revaluation and Course registration </p><hr>
+					<p class="normal link ">UG/PG : Answer scripts revaluation and Course registration </p><hr>
+					<p class="seading ease">More Notices ></p>
+
+				</div>
+
+				<div class="bar">
+				<p class="heading orange">EVENTS </p><hr>
+					<div class="din normal  "> <div class="date"><div>JAN.</div>15.</div>  <span class="link" style="margin-left:10px;"> MDP on Knowledge Management & Innovation, 29-31 January 2015 > </span></div><hr>
+					<div class="din normal  "> <div class="date"><div>JAN.</div>15.</div>  <span class="link" style="margin-left:10px;"> MDP on Knowledge Management & Innovation, 29-31 January 2015 > </span></div><hr>
+					<div class="din normal  "> <div class="date"><div>JAN.</div>15.</div>  <span class="link" style="margin-left:10px;"> MDP on Knowledge Management & Innovation, 29-31 January 2015 > </span></div><hr>
+					<p class="seading ease">More Events ></p>
+
+				</div>
+
+				<div class="bar">
+				<p class="heading orange">WELCOME </p><hr>
+					<p class="normal link ">COMPUTER CENTER <span style="float:right">></span></p><hr>
+					<p class="normal link ">TEQIP II <span style="float:right">></span></p><hr>
+					<p class="normal link ">SPORTS CENTER <span style="float:right">></span></p><hr>
+					<p class="normal link ">LIBRARY <span style="float:right">></span></p><hr>	
+				</div>
+
+			</div>
+			<div class="pfooter"></div>
+		</center>
+
+	</div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,114 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script type="text/javascript" src="script/jquery.js" ></script>
+	<script type="text/javascript" src="script/bootstrap.min.js" ></script>
+	<link rel="stylesheet" href="css/responsivemobilemenu.css" type="text/css">
+
+<script type="text/javascript" src="script/responsivemobilemenu.js"></script>
+</head>
+<body>
+	<div class ="wrapper">
+		
+		
+		<img class='img-responsive' src="img/slider1.png" style="background:#2d2d2f;display: block;max-width: 100%;width: 100%;height: auto;" />		<center>
+		<div class="rmm minimal" data-menu-style="minimal" style="z-index:999;max-width: 100%;background:2d2d2f;color:#000;">
+        <ul class="rmm-main-list" style="display: block;background:2d2d2f;color:#000;">
+            <li><a href="http://responsivemobilemenu.com/demo/#home">Home</a></li>
+            <li><a href="http://responsivemobilemenu.com/demo/#about-me">About me</a></li>
+            <li><a href="http://responsivemobilemenu.com/demo/#gallery">Gallery</a></li>
+            <li><a href="http://responsivemobilemenu.com/demo/#blog">Blog</a></li>
+            <li><a href="http://responsivemobilemenu.com/demo/#links">Links</a></li>
+            <li><a href="http://responsivemobilemenu.com/demo/#sitemap">Sitemap</a></li> 
+       </ul>
+</div>
+			<div class="mcon">
+				<div class="bar">
+				<div class="vision">
+					<center>Vision</center>
+					<p>To provide valuable resources for industry and society through excellence in technical education and research.</p>
+					
+					</div>
+					<div class="mission">
+					<center>Mission<center>
+					<hr>
+					<p>
+					
+					To offer state-of-the-art undergraduate, postgraduate and doctoral programmes.</p><hr><p>
+					To generate new knowledge by engaging in cutting-edge research.</p><hr><p>
+					To undertake collaborative projects with academia and industries.</p><hr><p>
+					To develop human intellectual capability to its fullest potential.</p>
+				</div>
+				</div>
+
+				<div class="bbar">
+				<div class="story">
+				<p class="seading ease">Read Top News ></p><hr>
+				<div class="bar">
+					<div class="news bborder">
+					<p class="gbar">Founder's Desk</p>
+<p class="ibar">We have extensive experience designing interiors for restaurants and offices. Cras mattis consectetur purus sit amet fermentum. Cum sociis natoque penatibus et magnis dolor sit amet.</p>
+					</div>
+				</div>
+				<div class="bar">
+					<div class="news bborder">
+					<p class="gbar">Founder's Desk</p>
+<p class="ibar">We have extensive experience designing interiors for restaurants and offices. Cras mattis consectetur purus sit amet fermentum. Cum sociis natoque penatibus et magnis dolor sit amet.</p>
+					</div>
+				</div><br>
+				<div class="bar">
+					<div class="news bborder">
+					<p class="gbar">Founder's Desk</p>
+<p class="ibar">We have extensive experience designing interiors for restaurants and offices. Cras mattis consectetur purus sit amet fermentum. Cum sociis natoque penatibus et magnis dolor sit amet.</p>
+					</div>
+				</div>
+				<div class="bar">
+					<div class="news bborder">
+					<p class="gbar">Founder's Desk</p>
+<p class="ibar">We have extensive experience designing interiors for restaurants and offices. Cras mattis consectetur purus sit amet fermentum. Cum sociis natoque penatibus et magnis dolor sit amet.</p>
+					</div>
+				</div>
+				</div>
+				</div>
+
+			</div>
+			<div class="scon">
+
+				<div class="bar">
+					<p class="heading orange">NOTICES/DOWNLOADS </p><hr>
+					<p class="normal link ">UG/PG : Answer scripts revaluation and Course registration </p><hr>
+					<p class="normal link ">UG/PG : Answer scripts revaluation and Course registration </p><hr>
+					<p class="normal link ">UG/PG : Answer scripts revaluation and Course registration </p><hr>
+					<p class="normal link ">UG/PG : Answer scripts revaluation and Course registration </p><hr>
+					<p class="seading ease">More Notices ></p>
+
+				</div>
+
+				<div class="bar">
+				<p class="heading orange">EVENTS </p><hr>
+					<div class="din normal  "> <div class="date"><div>JAN.</div>15.</div>  <span class="link" style="margin-left:10px;"> MDP on Knowledge Management & Innovation, 29-31 January 2015 > </span></div><hr>
+					<div class="din normal  "> <div class="date"><div>JAN.</div>15.</div>  <span class="link" style="margin-left:10px;"> MDP on Knowledge Management & Innovation, 29-31 January 2015 > </span></div><hr>
+					<div class="din normal  "> <div class="date"><div>JAN.</div>15.</div>  <span class="link" style="margin-left:10px;"> MDP on Knowledge Management & Innovation, 29-31 January 2015 > </span></div><hr>
+					<p class="seading ease">More Events ></p>
+
+				</div>
+
+				<div class="bar">
+				<p class="heading orange">WELCOME </p><hr>
+					<p class="normal link ">COMPUTER CENTER <span style="float:right">></span></p><hr>
+					<p class="normal link ">TEQIP II <span style="float:right">></span></p><hr>
+					<p class="normal link ">SPORTS CENTER <span style="float:right">></span></p><hr>
+					<p class="normal link ">LIBRARY <span style="float:right">></span></p><hr>	
+				</div>
+
+			</div>
+			<div class="pfooter"
+		</center>
+
+	</div>
+
+</body>
+</html>
